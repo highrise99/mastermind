aa0b672fcc6bc6d631e742ad30c2b02c47c69daa
Highrise99 <highrise2357@gmail.com>
2013-07-05 21:05:06 -0400

    deleted old messages files

--- mastermind/messages.cpp
+++
@@ -1,6 +0,0 @@
-#include "messages.h"
-
-MESSAGES::MESSAGES() : GREAT("    error : invalid input\n\n"), LESS("    error : invalid input\n\n"), PROMPT("input: ") {}
-
-MESSAGES::MESSAGES(std::string CONSTRUCTOR_GREAT, std::string CONSTRUCTOR_LESS, std::string CONSTRUCTOR_PROMPT) : GREAT(CONSTRUCTOR_GREAT), LESS(CONSTRUCTOR_LESS),
-        PROMPT(CONSTRUCTOR_PROMPT) {}
--- mastermind/messages.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-struct MESSAGES {
-    std::string GREAT; // the error code displayed if the input is too large
-    std::string LESS; // "[. . .]" small
-    std::string PROMPT;
-    MESSAGES();
-    MESSAGES(std::string, std::string, std::string);
-};
